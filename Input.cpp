08a669541652b49517565324017047d8c43588e9
siam <bdshiam330@gmail.com>
2026-07-02 06:32:59 +0600

    Create Input.cpp

---
+++ Input.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num1,num2,num3;
+char ch;
+ch='a';
+num1=1,num2=2,num3=3;
+cout<<"first number is:"<<num1<<endl<<"secend number is:"<<num2 <<endl <<"third number is:"<<num3<<endl;
+cout<<"the char is:"<<ch;
+
+
+}
