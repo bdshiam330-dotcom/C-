a20c240c5902ad5f34e45ee5eb0822109bcaac64
siam <bdshiam330@gmail.com>
2026-07-02 06:50:01 +0600

    Implement sizeof operator demonstration in SpecialOperator.cpp
    Add a program to demonstrate sizeof operator for various data types

---
+++ SpecialOperator.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+double b;
+float f;
+char c;
+char name[20];
+int d=sizeof(a);
+cout<<d<<endl;
+ d=sizeof(b);
+cout<<d<<endl;
+ d=sizeof(f);
+cout<<d<<endl;
+ d=sizeof(c);
+cout<<d<<endl;
+ d=sizeof(name);
+cout<<d;
+
+
+
+}
