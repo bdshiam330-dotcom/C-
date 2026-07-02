96696796613a52211f0fd548be4be0daaf51b29b
siam <bdshiam330@gmail.com>
2026-07-02 06:30:47 +0600

    Add HellowWord.cpp to print user information

---
+++ HellowWord.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"Hellow word!\n";
+cout<<"name:siam\n";
+cout<<"home: bogura\n";
+cout<<"siam islam"<<endl<<"hellow word"<<endl<<"home:bogura";
+
+}
