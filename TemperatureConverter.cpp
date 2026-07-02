9d2fa4179870ab2401a5cacbb7902d098f6123a0
siam <bdshiam330@gmail.com>
2026-07-02 06:40:24 +0600

    Add TemperatureConverter for Celsius to Fahrenheit and Kelvin
    Implement temperature conversion from Celsius to Fahrenheit and Kelvin.

---
+++ TemperatureConverter.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+double far,cell,cells,kel;
+cout<<"Enter celcious:";
+cin>>cell;
+far=1.8*cell+32;
+cout<<"the far is:"<<far<<endl;
+cells=(far-32)/1.8;
+cout<<"the cells is:"<<cells<<endl;
+kel=cells+273;
+cout<<"the kelvin is:"<<kel;
+
+
+
+}
+
+
