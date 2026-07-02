25282cecfd8d3d4811658382099d8ff384b0e2be
siam <bdshiam330@gmail.com>
2026-07-02 06:44:45 +0600

    Implement assignment operators in AssignmentOperator.cpp
    Demonstrate various assignment operators in C++.

---
+++ AssignmentOperator.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x=5;
+int y=10;
+x+=y;//x=x+y
+cout<<x<<endl;
+x-=y;
+cout<<x<<endl;
+x*=y;
+cout<<x<<endl;
+x/=y;
+cout<<x<<endl;
+x%=y;//x=x+y
+cout<<x;
+
+
+
+
+
+
+}
