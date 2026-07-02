2f6d65ae40a0999ec8d1e46a75104b3a77005118
siam <bdshiam330@gmail.com>
2026-07-02 06:33:59 +0600

    Implement basic arithmetic operations in String.cpp

---
+++ String.cpp
@@ -0,0 +1,24 @@
+include<iostream>
+using name#space std;
+int main(){
+int num1,num2,result;
+cout<<"Enter two numbers:";
+cin>> num1>>num2;
+cout<<showpoint;
+ result=num1+num2;
+ cout<<"the sum is:"<<result;
+ cout<<endl;
+result=num1-num2;
+ cout<<"the sub is:"<<result;
+ cout<<endl;
+ result=num1*num2;
+ cout<<"the mul is:"<<result;
+ cout<<endl;
+double div=(float)num1/num2;//type casting
+ cout<<"the div is:"<<div<<endl;
+ result=num1%num2;
+ cout<<"the mod is:"<<result;
+
+
+
+}
