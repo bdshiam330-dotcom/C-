bb72d87d75657435e717337bd5975360dc5ab165
siam <bdshiam330@gmail.com>
2026-07-02 06:38:19 +0600

    Add basic arithmetic operations in ArthmaticOperator.cpp
    Implement basic arithmetic operations: addition, subtraction, multiplication, division, and modulus.

---
+++ ArthmaticOperator.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num1,num2,result;
+cout<<"Enter two numbers:";
+cin>>num1>>num2;
+ result=num1+num2;
+cout <<"the sum is:"<<result<<endl;
+ result=num1-num2;
+cout <<"the sub is:"<<result<<endl;
+ result=num1/num2;
+cout <<"the div is:"<<result<<endl;
+ result=num1*num2;
+cout <<"the mul is:"<<result<<endl;
+ result=num1%num2;
+cout <<"the mod is:"<<result;
+
+
+
+}
